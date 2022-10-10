33c47582ee66effbd78e7a0e4389a58a4ab3ce8a
Alonso <alonsogomezcasanova@gmail.com>
2022-10-10 20:06:18 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola todos!, Nueva version 2.1 disponible!!");
+        System.out.println("Hola todoskjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj!, Nueva version 2.1 disponible!!");
+
     }
 }
